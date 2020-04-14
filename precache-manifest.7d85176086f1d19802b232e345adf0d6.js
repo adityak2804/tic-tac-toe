1630783d70ba34b1a89a03eaeeedf42059b0d826
Aditya Kumar <adityak2804@gmial.com>
2020-04-15 03:21:21 +0530

    Updates

---
+++ precache-manifest.7d85176086f1d19802b232e345adf0d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8baa2b59419c40267111b8f27c3e11f8",
+    "url": "/adityak2804/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "5ff7b9e2198b8701dabf",
+    "url": "/adityak2804/tic-tac-toe/static/css/2.8f8036f3.chunk.css"
+  },
+  {
+    "revision": "de4a042b9af1eb1aed03",
+    "url": "/adityak2804/tic-tac-toe/static/css/main.070e8683.chunk.css"
+  },
+  {
+    "revision": "5ff7b9e2198b8701dabf",
+    "url": "/adityak2804/tic-tac-toe/static/js/2.244c22d1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/adityak2804/tic-tac-toe/static/js/2.244c22d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4a042b9af1eb1aed03",
+    "url": "/adityak2804/tic-tac-toe/static/js/main.8fcbc28d.chunk.js"
+  },
+  {
+    "revision": "31f1468edc68d52eaae1",
+    "url": "/adityak2804/tic-tac-toe/static/js/runtime-main.c88e160b.js"
+  }
+]);
